daa0b8c3c6dd6708ad8a4043a4b85be81f53fc66
mzahin01 <minhazahin@gmail.com>
2024-09-12 20:52:15 +0600

    ...

--- lib/presentation/widgets/body/projects/project_item.dart
+++ lib/presentation/widgets/body/projects/project_item.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-
 import '../../../../core/utils/app_colors.dart';
 import '../../../../core/utils/app_styles.dart';
 import '../../../../data/models/project.dart';
